ecae997f809079e6b4cd7629705e6e1266c2cd2c
Pham Huu Phat <phatph.work@gmail.com>
2025-01-04 23:04:42 +0700

    hiển thị dữ liệu firebase

--- lib/features/task_manager/presentation/srceen/task.dart
+++ lib/features/task_manager/presentation/srceen/task.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:task_manager/features/task_manager/presentation/srceen/task_create.dart';
 
@@ -29,6 +30,26 @@ class TaskPage extends StatelessWidget {
         },
         child: const Icon(Icons.add),
       ),
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection('tasks').snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            final tasks = snapshot.data?.docs ?? [];
+            return ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                final task = tasks[index];
+                return ListTile(
+                  title: Text(task['name']),
+                  subtitle: Text(task['date']),
+                );
+              },
+            );
+          }),
     );
   }
 }
